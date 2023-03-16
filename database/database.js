904770897225ae968642ead7c7804b24e87aee96
Andrey Rodriguez <andrey.rgz.2016@gmail.com>
2023-03-16 01:24:51 -0400

    MOD: agregados las variables de entorno

--- database/database.js
+++ database/database.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
-
-
+import * as dotenv from 'dotenv'
+dotenv.config()
+const PORT = process.env.PORT || 3001
+const MONGO_URL = process.env.MONGO_URL || "mongodb://localhost:3001/loginChat"
 // Conexión a la base de datos
-const mongoConnection = () => mongoose.connect('mongodb://localhost:3001/loginChat', {
+const mongoConnection = () => mongoose.connect(MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
-module.exports = { mongoConnection };
+module.exports = { mongoConnection, PORT };
